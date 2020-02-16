4fc1eb097c00b0eb6064fe8e2036730da589c24a
Brennan Roel <brennanroel@gmail.com>
2020-02-16 15:20:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == 'I LOVE YOU GRANDMA!'
-   print 'I LOVE YOU TOO PUMPKIN!'
+   'I LOVE YOU TOO PUMPKIN!'
    elsif != phrase.upcase
-   print 'HUH?! SPEAK UP, SONNY!'
+   'HUH?! SPEAK UP, SONNY!'
   else 'NO, NOT SINCE 1938!'
   end
  end
