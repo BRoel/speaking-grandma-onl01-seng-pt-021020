37f19cb873c7aceb02a464feba977e03dda6b0a3
Brennan Roel <brennanroel@gmail.com>
2020-02-16 15:22:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,13 @@
-def speak_to_grandma(phrase)
-  if phrase == 'I LOVE YOU GRANDMA!'
-   'I LOVE YOU TOO PUMPKIN!'
-   elsif != phrase.upcase
-   'HUH?! SPEAK UP, SONNY!'
-  else 'NO, NOT SINCE 1938!'
+def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase == "Hi Nana, how are you?"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == "WHAT?"
+    return "NO, NOT SINCE 1938!"
   end
- end
-   
+end
